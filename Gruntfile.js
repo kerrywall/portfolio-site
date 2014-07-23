2d2b77fbaa23735986e0093896bb3c3f25b0a68b
kerrywall <kerry.wall@gmail.com>
2014-07-23 14:50:58 -0400

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,47 @@
+module.exports = function(grunt) {
+
+ // Project configuration.
+ grunt.initConfig({
+   pkg: grunt.file.readJSON('package.json'),
+   sass: {
+   		dist: {
+   			files: {
+   				'css/style.css':'css/style.scss'
+   			}
+   		}
+   },
+   watch: {
+   	css: {
+   		 files: ['css/*.scss'],
+   		 tasks: ['sass', 'autoprefixer']
+   	},
+   	options: {
+   		liveReload: true
+   	}
+   },
+   autoprefixer: {
+   	options: {
+   		browsers: ['last 5 version', 'ie 7', 'ie 8', 'ie 9']
+   	},
+   	no_dest: {
+   		src: 'css/style.css'
+   	}
+   },
+   connect: {
+   	server: {
+   		options: {
+   			port: 9001,
+   			base: ''
+   		}
+   	}
+   }
+ });
+
+ grunt.loadNpmTasks('grunt-contrib-sass');
+ grunt.loadNpmTasks('grunt-contrib-watch');
+ grunt.loadNpmTasks('grunt-autoprefixer');
+ grunt.loadNpmTasks('grunt-contrib-connect');
+
+ // Default task(s).
+ grunt.registerTask('default', ['connect','watch']);
+};
