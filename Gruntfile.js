0d0b42fcd1800194b2f25f3c83d1dc5daf7ed180
kerrywall <kerry.wall@gmail.com>
2014-07-25 15:42:39 -0400

    All set and responsivated.

--- Gruntfile.js
+++ Gruntfile.js
@@ -34,14 +34,25 @@ module.exports = function(grunt) {
    			base: ''
    		}
    	}
-   }
+   },
+   imagemin: {                          // Task
+       dynamic: {                         // Another target
+             files: [{
+               expand: true,                  // Enable dynamic expansion
+               cwd: 'images/',                   // Src matches are relative to this path
+               src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+               dest: 'images/'                  // Destination path prefix
+             }]
+           }
+     }
  });
 
  grunt.loadNpmTasks('grunt-contrib-sass');
  grunt.loadNpmTasks('grunt-contrib-watch');
  grunt.loadNpmTasks('grunt-autoprefixer');
  grunt.loadNpmTasks('grunt-contrib-connect');
+ grunt.loadNpmTasks('grunt-contrib-imagemin');
 
  // Default task(s).
- grunt.registerTask('default', ['connect','watch']);
+ grunt.registerTask('default', ['connect','watch', 'imagemin']);
 };
