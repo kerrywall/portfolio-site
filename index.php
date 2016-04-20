064d38bd8593d56947fc9036daf029db83f03190
kerrywall <kerry.wall@gmail.com>
2016-04-19 22:25:20 -0400

    pulling skills and blog

--- index.php
+++ index.php
@@ -23,15 +23,6 @@ $portfolioQuery = new WP_Query(
 		)
 	);
 
-// query the blog posts
-$blogQuery = new WP_Query(
-	array(
-		'posts_per_page' => 4,
-		'post_type' => 'post',
-		'order_by' => 'menu_order',
-		'order' => 'DSC'
-		)
-	);
  ?>
 
 <!-- call in the pages -->
@@ -105,54 +96,6 @@ $blogQuery = new WP_Query(
 	[stuff that happens if there aren't any posts]
 <?php endif; ?>
 <!-- end calling in the portfolio items -->
-
-<!-- call in the blog posts -->
-<?php if ( $blogQuery->have_posts() ) : ?>
-
-	<section id="blog" class="divider clearfix theBlog">
-		<div class="content">
-			<div class="logo">
-				<span class="fa fa-pencil"></span>
-			</div>
-				<h2>Blog</h2>
-
-			<?php while ($blogQuery->have_posts()) : $blogQuery->the_post(); ?>
-
-				<section id="<?php echo $post->post_name; ?>" class="<?php echo $post->post_name; ?>">
-					<div class="images">
-					  <a href="<?php echo get_permalink(); ?>">
-					    <div class="image">
-					      <?php $image = the_post_thumbnail(); ?>
-					        <!-- <img src="<?//php echo $image['sizes']['medium'] ?>"> -->
-					    </div>
-					</div>
-					<div class="info">
-					    	<p class="date">
-					    		<span class="day"><?php echo get_the_date('d'); ?></span>
-					    		<span class="month"><?php echo get_the_date('F'); ?></span>
-					    		<span class="year"><?php echo get_the_date('Y'); ?></span>
-					    	</p>
-					    	<div class="excerpt">
-								<a href="<?php echo get_permalink(); ?>">
-									<h3 class="title"><?php the_title(); ?></h3>
-								</a>
-					    		<p><?php echo get_the_excerpt(); ?></p>
-					    	</div>
-					    	<p class="tags"><?php the_tags(); ?></p>
-					    </div>
-				</section>
-			<?php endwhile; ?>
-			
-			<?php wp_reset_postdata(); ?>
-		</div>
-		
-
-<?php elseif ( $blogQuery->have_posts() ) : ?>
-	
-<?php else:  ?>
-	[stuff that happens if there aren't any posts]
-<?php endif; ?>
-<!-- end calling in the blog posts -->
 
 <!-- get the footer -->
 <?php get_footer(); ?>
